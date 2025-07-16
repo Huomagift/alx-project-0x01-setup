a854a9fdb0b33dc5f8157310da1155c32afd9a14
Huomagift <huomagift@gmail.com>
2025-07-16 23:11:15 +0100

    Set up Header component

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/layout/Header"
 const posts: React.FC = () => {
     return (
         <div className="flex justify-center items-center h-screen">
             <h1 className="text-7xl font-thin">posts</h1>
+            <Header />
         </div>
     )
 } 
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/layout/Header"
 const users: React.FC = () => {
     return (
         <div className="flex justify-center items-center h-screen">
             <h1 className="text-7xl font-thin">users</h1>
+            <Header />
         </div>
     )
 } 
